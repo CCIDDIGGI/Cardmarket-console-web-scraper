90e493075945fff139035dd27a83801206695d4f
CCIDDIGGI <marco.boccolacci@gmail.com>
2024-01-23 11:59:44 +0100

    Init commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+namespace program
+{
+  class CardmarketWebScraper
+    {
+        static void Main(String[] args)
+        {
+            //init commit
+            Console.WriteLine("Initial commit!");
+            Console.ReadLine();
+        }
+    }
+}
